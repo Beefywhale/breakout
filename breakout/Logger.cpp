7106ae6a1f46390b36542cad7557d90b2ad73011
Jonathan <jonathan@integritypts.com>
2018-05-17 19:48:38 -0500

    String fix

--- breakout/Logger.cpp
+++ breakout/Logger.cpp
@@ -8,7 +8,7 @@ void Logger::error(std::string err) {
 	std::time_t t = std::time(0);
 	std::tm* now = std::localtime(&t);
 	char buf[255];
-	strftime(buf, sizeof(buf), "[%H:%M %p] ERROR -", now);
+	strftime(buf, sizeof(buf), "[%H:%M %p] ERROR - ", now);
 	std::string time(buf);
 	fprintf(stderr, (time+" "+err+"\n").c_str());
 }
