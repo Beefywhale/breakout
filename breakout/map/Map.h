5375c24c6621ab1bd5924cb6c2f41bbbdc64e7dc
Beefywhale <beefywhale@gmail.com>
2018-05-27 14:27:13 -0500

    Replaced vector of tiles to a map with x,y pair key and Tile value.

--- breakout/map/Map.h
+++ breakout/map/Map.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <vector>
+#include <map>
+#include <utility>
 #include "../global.h"
 #include <SFML/Graphics.hpp>
 #include "Tile.h"
@@ -13,10 +15,10 @@ public:
     Map();
 	
    /**
-	* Get all of the Map's Tiles in a vector.
-	* @return a vector of Tiles.
+	* Get all of the Map's Tiles in a std::map.
+	* @return a std::map with x,y pair key and Tile value.
 	*/
-    std::vector<Tile> getTiles() { return tileMap; }
+    std::map<std::pair<int, int>, Tile> getTiles() { return tileMap; }
 
    /**
 	* Get the Tile at a specific set of coordinates
@@ -26,6 +28,7 @@ public:
 	*/
     Tile getTileAt(int x, int y);
 private:
-    std::vector<Tile> tileMap;
+    std::map<std::pair<int, int>, Tile> tileMap;
+    std::map<std::pair<int, int>, Tile>::iterator it;
     Logger logger;
 };
