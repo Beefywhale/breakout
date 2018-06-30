33fed2489e8f916ccdfae9c2c60bb4d1dd09f346
Beefywhale <beefywhale@gmail.com>
2018-06-30 17:11:43 -0500

    added the Type.h file

---
+++ breakout/map/Type.h
@@ -0,0 +1,31 @@
+#pragma once
+
+class Type {
+public:
+    struct DoorType {
+        bool open;
+    };
+
+    struct FloorType {};
+    struct WallType {};
+
+    Type() {}
+
+    enum Types {
+        Door,
+        Floor,
+        Wall,
+        
+        Count
+    };
+
+    Types type;
+
+
+    union {
+        DoorType door;
+        FloorType floor;
+        WallType wall;
+    };
+
+};
